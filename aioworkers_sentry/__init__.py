c3cb857c98e7b43a5042e957116cc7c3eca52f6d
Alexander Malev <yttrium@somedev.ru>
2023-04-12 00:17:51 +0300

    fix default __version__

--- aioworkers_sentry/__init__.py
+++ aioworkers_sentry/__init__.py
@@ -4,7 +4,7 @@ try:
     # true
     from .version import __version__
 except ImportError:
-    __version__ = 'dev'
+    __version__ = '0.0.0.dev0'
 
 
 configs = Path(__file__).parent / 'sentry.ini',
