c87c61e240bff4e0032be1555f0f2a8c4427ab44
Alexander Malev <aamalev@users.noreply.github.com>
2023-05-06 13:09:29 +0400

    improve config params (#93)
    * improve config params
    
    * compatibility 0.20

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,6 +1,39 @@
 import logging
 
+import pytest
+from aioworkers.core.config import ValueExtractor
 from sentry_sdk import Hub
+
+from aioworkers_sentry.client import Sentry
+
+
+async def test_config_exc(context):
+    sentry = Sentry(context=context)
+
+    with pytest.raises(ValueError):
+        sentry.set_config(
+            ValueExtractor(
+                {
+                    "integrations": [1],
+                }
+            )
+        )
+
+
+def sampler(x):  # no cov
+    return 1
+
+
+async def test_config_factory(context):
+    sentry = Sentry(context=context)
+
+    sentry.set_config(
+        ValueExtractor(
+            {
+                "traces_sampler": "tests.test_client.sampler",
+            }
+        )
+    )
 
 
 async def test_capture_exc(catch_sentry):
@@ -16,5 +49,5 @@ async def test_capture_exc(catch_sentry):
 
 async def test_logging(catch_sentry):
     assert not catch_sentry
-    logging.getLogger().critical('error')
+    logging.getLogger().critical("error")
     assert catch_sentry
